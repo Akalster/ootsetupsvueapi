8ec8524eea82880754110d4d8ca8f9dd7f67bc97
JAMNESIA <jamiesurowiec@hotmail.com>
2021-12-13 15:16:36 +0100

    first commit

---
+++ helper.spec.js
@@ -0,0 +1,17 @@
+// reads the .env file and stores it as environment variables, use for config
+require("dotenv").config();
+
+const connect = require("./connect");
+
+//IMPORT MODELS HERE
+
+// const User = require("./src/models/user.model")(); // note we need to call the model caching function
+// const Product = require("./src/models/product.model")(); // note we need to call the model caching function
+
+// connect to the databases
+connect.mongo(process.env.MONGO_TEST_DB);
+
+beforeEach(async () => {
+  // drop both collections before each test
+  //await Promise.all([User.deleteMany(), Product.deleteMany()]);
+});
