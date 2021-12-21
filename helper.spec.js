e733da9730db87d01257a44c3e626b0318ad682e
Jop Rill <joprillmail@gmail.com>
2021-12-21 14:05:34 +0100

    review crud

--- helper.spec.js
+++ helper.spec.js
@@ -1,17 +1,17 @@
 // reads the .env file and stores it as environment variables, use for config
-require("dotenv").config();
+require('dotenv').config();
 
-const connect = require("./connect");
+const localconnect = require('./localconnect');
 
 //IMPORT MODELS HERE
 
-const User = require("./src/models/user.model")(); // note we need to call the model caching function
+const User = require('./src/models/user.model')(); // note we need to call the model caching function
 // const Product = require("./src/models/product.model")(); // note we need to call the model caching function
 
 // connect to the databases
-connect.mongo(process.env.MONGO_TEST_DB);
+localconnect.mongo(process.env.MONGO_TEST_DB);
 
 beforeEach(async () => {
-  // drop both collections before each test
-  await Promise.all([User.deleteMany()]);
+    // drop both collections before each test
+    await Promise.all([User.deleteMany()]);
 });
