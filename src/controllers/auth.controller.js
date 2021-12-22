d3c40e8bdb0063424827a6c7181a288b46ec4f6c
JAMNESIA <jamiesurowiec@hotmail.com>
2021-12-22 11:49:40 +0100

    getting id's for question out of the routes

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -15,28 +15,6 @@ async function register(req, res) {
 }
 
 async function login(req, res) {
-    // // check whether request is valid
-    // if(!req.body.user) {
-    //     throw new errors.EntityNotFoundError('User is required to purchase a product')
-    // }
-
-    // // get the product from the db and check whether we have such a product
-    // const product = await Product.findById(req.params.id)
-    // if(!product) {
-    //     throw new errors.EntityNotFoundError(`Product with id '${req.params.id}' not found`)
-    // }
-
-    // // add the product to the bought list of the user
-    // const user = await User.findOne({name: req.body.user})
-
-    // // maybe not necessary any more now that we store it in neo?
-    // // BEWARE: atomicity issues!
-    // user.bought.push(product._id)
-    // await user.save()
-
-  
-
-    // res.status(201).end()
     const userProps = req.body;
     const user = await User.findOne({ email: userProps.email });
 
