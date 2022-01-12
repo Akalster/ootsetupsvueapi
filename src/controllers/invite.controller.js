8c19e4ee6e4c62dccd12e7d660ecf353f92801df
Jop Rill <joprillmail@gmail.com>
2022-01-12 02:40:16 +0100

    Inviting players via mail

---
+++ src/controllers/invite.controller.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+    service: 'hotmail',
+    auth: {
+        user: process.env.MAILUSER,
+        pass: process.env.MAILPASS,
+    },
+    tls: {
+        rejectUnauthorized: false,
+    },
+});
+
+async function sendmailInvite(req, res) {
+    const userProps = req.body;
+    const sender = userProps.sender;
+    const recipients = userProps.recipients;
+    const reviewid = userProps.reviewid;
+
+    let toList = recipients.join(',');
+
+    const options = {
+        from: 'ihomertest@hotmail.com',
+        to: toList,
+        subject: 'Review invitation from ' + sender,
+        html:
+            '<h2>' +
+            sender +
+            ' wants you to review him! Use the link below</h2><a href="http://localhost:8080/#/reviews/' +
+            reviewid +
+            '">Review</a>',
+    };
+
+    transporter.sendMail(options, function (err, info) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log('Sent: ' + info.response);
+    });
+}
+
+module.exports = {
+    sendmailInvite,
+};
--- src/routes/review.routes.js
+++ src/routes/review.routes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const Review = require('../models/review.model')(); // note we need to call the model caching function
+const User = require('../models/user.model')(); // note we need to call the model caching function
 
 const CrudController = require('../controllers/crud');
+const InviteController = require('../controllers/invite.controller');
 
 const ReviewCrudController = new CrudController(Review);
+const UserCrudController = new CrudController(User);
 
 // get all reviews
 router.get('/', ReviewCrudController.getAll);
@@ -21,5 +24,8 @@ router.put('/:id', ReviewCrudController.update);
 
 // remove a review
 router.delete('/:id', ReviewCrudController.delete);
+
+// get a review and invite
+router.post('/:id/invite', InviteController.sendmailInvite);
 
 module.exports = router;
